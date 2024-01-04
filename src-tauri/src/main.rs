7bddc7f6ceef2fe32cfe82302493c3fc634811d5
lemonilemon <imlemonilemon@gmail.com>
2024-01-04 14:25:28 +0000

    test menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,11 +4,23 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {}! You haven't been greeted from Rust!", name)
 }
 
+
+
 fn main() {
+    use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+    let menu = Menu::new()
+       .add_native_item(MenuItem::Copy)
+       .add_item(CustomMenuItem::new("hide", "Hide"))
+       .add_submenu(submenu);
+
     tauri::Builder::default()
+        .menu(menu)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
