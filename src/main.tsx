a2fa6ac39a223a92faa2ac0f5fb1191401000f7d
AaronWu-train <aaronwu0218@gmail.com>
2024-01-22 21:57:34 +0800

    temporary update

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 // import node modules
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {NextUIProvider} from "@nextui-org/react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // import main style
-import "./index.css";
+// import "./index.css";
 
 // import page sections
 // import Header from "./header/Header";
@@ -13,6 +15,10 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <main className="mocha text-foreground bg-background">
+        <App />
+      </main>
+    </NextUIProvider>
   </React.StrictMode>
 );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+
+function createNewProject() {
+    // TODO: Create a new project
+}
+
+function openProject() {
+    // TODO: Open a new project
+}
 
 const Home: React.FC = () => {
     return (
         <div>
-            <h1>Welcome to the Home Page</h1>
-            {/* Add your content here */}
-        </div>
+            <div className="container">
+                
+            <div className="text-center p-4 p-lg-5">
+                <p className="fw-bold text-primary mb-2">
+                    A tool to generate testcase for competitive programming
+                </p>
+                
+                <h1 className="fw-bold mb-4">MochaGen</h1>
+                <button className="btn btn-primary fs-5 me-2 py-2 px-4" type="button">
+                    <Link to="/description">New Project</Link>            
+                </button>
+                <button className="btn btn-light fs-5 py-2 px-4" type="button">
+                    <Link to="/description">Open An Exist Project</Link>
+                </button>
+            </div>
+            </div>
+            
+            
+        </div>  
     );
-};
+}
 
 export default Home;
