201d1b27d346c49424c9d1003bb003d01759fc82
AaronWu-train <aaronwu0218@gmail.com>
2024-02-01 08:54:55 +0800

    temp-setting
    暫時commit

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,36 +1,69 @@
-import React, { useState } from "react";
+import {
+  Container,
+  Button,
+  Grid,
+  Box,
+  Flex,
+  Heading,
+  Text,
+} from "@radix-ui/themes";
+import { platform } from "@tauri-apps/api/os";
+
+async function checkGppPath() {
+  const commandName = (await platform()) === "win32" ? "where" : "which";
+  const commandArgs = (await platform()) === "win32" ? ["g++.exe"] : ["g++"];
+}
+
+// 呼叫函式檢查 g++ 的路徑
+checkGppPath();
 
 const Settings: React.FC = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     // Handle form submission logic here
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <br />
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <Container m="6" size="3">
+        <form>
+          <div className="form-group">
+            <label htmlFor="cppComplierPath">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="cppComplierPath"
+              placeholder="Select a C++ compiler"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </Container>
+    </>
   );
 };
 
