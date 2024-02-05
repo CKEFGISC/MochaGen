1664c75903d4447f18dffa7fda275a0f5baa7e63
lemonilemon <imlemonilemon@gmail.com>
2024-02-05 12:59:17 +0800

    init Description

--- src/pages/description/Description.tsx
+++ src/pages/description/Description.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-
-const Description: React.FunctionComponent = () => {
+import { Grid, Box, Flex, Heading, Text } from "@radix-ui/themes";
+interface IProps {
+    setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+    setLog: React.Dispatch<React.SetStateAction<string>>;
+}
+export default function Description(props: IProps) {
     return (
-        <div>
-            <h1>Welcome to the Description Page</h1>
+        <>
+            <Flex
+                display = "flex"
+                direction= "column"
+                justify = "start"
+                align = "center"
+                wrap = "wrap"
+                gap = "4"
+                style={{
+                    height: "90vh",
+                    flex: "1",
+                }}
+            >
+                <Flex direction = "column">
+                    <Heading size = "8" align = "center">
+                        Description <br />
+                    </Heading>
+                    <Text align = "center">
+                        Add your description here. <br/> 
+                        (Only Markdown Syntax is available for now.) <br/>
+                    </Text>
+                </Flex>
             {/* Add your content here */}
-        </div>
+            </Flex>
+        </>
     );
 };
-
-export default Description;
