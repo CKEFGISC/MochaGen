3b412b71a2641fcdf0780c788ea11c4fd2808a99
lemonilemon <imlemonilemon@gmail.com>
2024-02-10 14:34:05 +0800

    init ProcessProvider

---
+++ src/utils/AppContextProvider.tsx
@@ -0,0 +1,19 @@
+// import context providers
+import { ProcessContextProvider } from "./navbar/ProcessContext";
+import { LoadContextProvider } from "./loading/LoadContext.tsx";
+
+interface IProps {
+    children: React.ReactNode;
+}
+
+const AppContextProvider = ({ children }: IProps) => {
+    return (
+        <ProcessContextProvider>
+            <LoadContextProvider>
+                {children}
+            </LoadContextProvider>
+        </ProcessContextProvider>
+    );
+}
+
+export default AppContextProvider;
