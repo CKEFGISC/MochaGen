add16c33f854c8fcc0b4644d9987be1f7861439f
AaronWu-train <aaronwu0218@gmail.com>
2024-02-15 12:38:33 +0800

    Created Blockly Editor Component
    With default js blocky
    TODO: create custom blocks & rearrange UI

---
+++ src/pages/project/token-editor/blockly/BlocklyEditor.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef } from "react";
+import * as Blockly from "blockly";
+import { blocks } from "./blocks/text";
+import { forBlock } from "./generators/javascript";
+import { javascriptGenerator } from "blockly/javascript";
+import { save, load } from "./serialization";
+import { toolbox } from "./toolbox";
+import "./blockly.css";
+
+export default function BlocklyEditor() {
+    const blocklyDivRef = useRef<HTMLDivElement | null>(null);
+    const codeDivRef = useRef<HTMLPreElement | null>(null);
+    const outputDivRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() => {
+        if (blocklyDivRef.current) {
+            let workspace = Blockly.inject(blocklyDivRef.current, {
+                toolbox,
+            });
+
+            // Register the blocks and generator with Blockly
+            Blockly.common.defineBlocks(blocks);
+            Object.assign(javascriptGenerator.forBlock, forBlock);
+
+            // Load the initial state from storage and run the code.
+            load(workspace);
+            runCode(workspace);
+
+            // Every time the workspace changes state, save the changes to storage.
+            workspace.addChangeListener((e) => {
+                if (e.isUiEvent) return;
+                if (workspace) save(workspace);
+            });
+
+            // Whenever the workspace changes meaningfully, run the code again.
+            workspace.addChangeListener((e) => {
+                if (
+                    e.isUiEvent ||
+                    e.type === Blockly.Events.FINISHED_LOADING ||
+                    (workspace && workspace.isDragging())
+                ) {
+                    return;
+                }
+                runCode(workspace);
+            });
+        }
+    }, []);
+
+    const runCode = (workspace: Blockly.WorkspaceSvg) => {
+        const code = javascriptGenerator.workspaceToCode(workspace);
+
+        if (codeDivRef.current) codeDivRef.current.textContent = code;
+        if (outputDivRef.current) outputDivRef.current.innerHTML = "";
+
+        try {
+            eval(code);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div id="editorContainer">
+            <div id="outputPane">
+                <pre id="generatedCode">
+                    <code ref={codeDivRef}></code>
+                </pre>
+                <div ref={outputDivRef} id="blocklyOutput"></div>
+            </div>
+            <div ref={blocklyDivRef} id="blocklyDiv"></div>
+        </div>
+    );
+}
