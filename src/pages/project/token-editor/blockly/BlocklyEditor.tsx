b8491879bf7697504adc0e13f9afc3140c736594
AaronWu-train <aaronwu0218@gmail.com>
2024-02-21 15:54:46 +0800

    Finish Creating blocks

--- src/pages/project/token-editor/blockly/BlocklyEditor.tsx
+++ src/pages/project/token-editor/blockly/BlocklyEditor.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import * as Blockly from "blockly";
-import { blocks } from "./blocks/text";
-import { forBlock } from "./generators/javascript";
-import { javascriptGenerator } from "blockly/javascript";
+import { blocks } from "./blocks/tokenblocks";
+import { tokenGenerator } from "./generators/token";
 import { save, load } from "./serialization";
 import { toolbox } from "./toolbox";
 import "./blockly.css";
@@ -10,19 +9,20 @@ import "./blockly.css";
 export default function BlocklyEditor() {
   const blocklyDivRef = useRef<HTMLDivElement | null>(null);
   const codeDivRef = useRef<HTMLPreElement | null>(null);
-  const outputDivRef = useRef<HTMLDivElement | null>(null);
   const workspaceRef = useRef<Blockly.WorkspaceSvg | null>(null);
+
+  // Register the blocks and generator with Blockly
+  Blockly.common.defineBlocks(blocks);
 
   useEffect(() => {
     const blocklyDiv = blocklyDivRef.current;
     if (blocklyDiv && !workspaceRef.current) {
-      console.log("GGG");
       const workspace = Blockly.inject("blocklyDiv", { toolbox });
       workspaceRef.current = workspace;
 
-      // Register the blocks and generator with Blockly
-      Blockly.common.defineBlocks(blocks);
-      Object.assign(javascriptGenerator.forBlock, forBlock);
+      workspace.registerButtonCallback("createNewIdPressed", () => {
+        Blockly.Variables.createVariableButtonHandler(workspace, () => {}, "panda");
+      });
 
       // Load the initial state from storage and run the code.
       load(workspace);
@@ -45,27 +45,18 @@ export default function BlocklyEditor() {
   }, [blocklyDivRef]);
 
   const runCode = (workspace: Blockly.WorkspaceSvg) => {
-    const code = javascriptGenerator.workspaceToCode(workspace);
-
+    const code = tokenGenerator.workspaceToCode(workspace);
     if (codeDivRef.current) codeDivRef.current.textContent = code;
-    if (outputDivRef.current) outputDivRef.current.innerHTML = "";
-
-    try {
-      eval(code);
-    } catch (error) {
-      console.error(error);
-    }
   };
 
   return (
     <div id="editorContainer">
+      <div ref={blocklyDivRef} id="blocklyDiv"></div>
       <div id="outputPane">
         <pre id="generatedCode">
           <code ref={codeDivRef}></code>
         </pre>
-        <div ref={outputDivRef} id="blocklyOutput"></div>
       </div>
-      <div ref={blocklyDivRef} id="blocklyDiv"></div>
     </div>
   );
 }
