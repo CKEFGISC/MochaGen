84386400aa0e3b5839597403d053f8e99e75dcc7
lemonilemon <imlemonilemon@gmail.com>
2024-02-27 01:23:31 +0800

    Init monaco-editor

--- src/pages/project/description/Description.tsx
+++ src/pages/project/description/Description.tsx
@@ -1,31 +1,41 @@
 import { Flex, Heading, Text } from "@radix-ui/themes";
-
+import Editor, { loader } from "@monaco-editor/react";
 export default function Description() {
-  return (
-    <>
-      <Flex
-        display="flex"
-        direction="column"
-        justify="start"
-        align="center"
-        wrap="wrap"
-        gap="4"
-        style={{
-          height: "90vh",
-          flex: "1",
-        }}
-      >
-        <Flex direction="column">
-          <Heading size="8" align="center">
-            Description <br />
-          </Heading>
-          <Text align="center">
-            Add your description here. <br />
-            (Only Markdown Syntax is available for now.) <br />
-          </Text>
-        </Flex>
-        {/* Add your content here */}
-      </Flex>
-    </>
-  );
+    loader.config({ paths: { vs: "/node_modules/monaco-editor/min/vs" } });
+    return (
+        <>
+            <Flex
+                display="flex"
+                direction="column"
+                justify="start"
+                align="center"
+                wrap="wrap"
+                gap="4"
+                style={{
+                    height: "90vh",
+                    flex: "1",
+                }}
+            >
+                <Flex direction="column">
+                    <Heading size="8" align="center">
+                        Description <br />
+                    </Heading>
+
+                    <Text align="center">
+                        Get your description ready. <br />
+                    </Text>
+
+                    <Editor
+                        height="70vh"
+                        width="80vw"
+                        theme="vs"
+                        defaultLanguage="markdown"
+                        defaultValue="Add your description here. (Only markdown syntax is available for now)"
+                    />
+
+                </Flex>
+                {/* Add your content here */}
+            </Flex>
+        </>
+    );
 }
