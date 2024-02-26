31f1ec22f875135147a8e38ae28bd9abc5a2b1c3
cj.tsai <jimtsai9596@gmail.com>
2024-02-26 22:23:50 +0800

    create file and write default code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,25 +2,32 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-/*
-const token_position: &str = "token.json";
-use serde_json::Value;
-use std;use serde_json::error;
-#[tauri::command]
-fn parse_token(project_directory: &str) -> Result<String, std::io::Error>{
-    let token_path = format!("{project_directory}{token_position}");
-    let contents = std::fs::read_to_string(token_path)?;
-    let contents_str : &str = contents.as_str();
-    //let contents = contents.as_str();
-    let tokens = serde_json::from_str(contents_str);
-    //println!("{}", tokens);
-    Ok("yeah".to_string())    
 
-}*/
+
+/*
+import { invoke } from '@tauri-apps/api/tauri'
+invoke('parse_token', { projectDirectory: "/Users/jimtsai/ytp/test" });
+*/
 use serde_json;
 use std::io;
 
 const TOKEN_POSITION: &str = "token.json";
+const GENERATOR_FORMAT: &str = "gen.cpp";
+const GENERATOR_POSITION: &str = "gen";
+const CPP_TEMPLATE_HEAD: &str = "
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);
+
+";
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+use std::fs::File;
+use std::io::{Write};
 
 #[tauri::command]
 fn parse_token(project_directory: &str) -> Result<String, String> {
@@ -30,17 +37,33 @@ fn parse_token(project_directory: &str) -> Result<String, String> {
         Err(e) => return Err(format!("Failed to read file: {}", e)),
     };
 
-    let tokens: serde_json::Result<serde_json::Value> = serde_json::from_str(&contents);
-    match tokens {
+    let tokens_all: serde_json::Result<serde_json::Value> = serde_json::from_str(&contents);
+    match tokens_all {
         Ok(parsed_json) => {
             println!("Parsed JSON: {:?}", parsed_json);
-            Ok("JSON parsed successfully".to_string())
+            if let Some(tokens_subtasks) = parsed_json["subtasks"].as_array() {
+                for tokens in tokens_subtasks {
+                    let subtask_id = tokens["subtask_id"].as_str().unwrap_or_default();
+                    let file_name = format!("{}/gen/sub{}_gen.cpp",project_directory,subtask_id);
+                    let mut file = match std::fs::File::create(&file_name) {
+                        Ok(file) => file,
+                        Err(e) => return Err(format!("Failed to create file '{}': {}", file_name, e)),
+                    };
+                    let code = CPP_TEMPLATE_HEAD.to_string();
+                    if let Err(e) = file.write_all(code.as_bytes()) {
+                        return Err(format!("Failed to write to file '{}': {}", file_name, e));
+                    }
+                }
+            } else {
+                return Err("Subtasks not found in JSON".to_string());
+            }
         }
         Err(e) => {
             eprintln!("Failed to parse JSON: {}", e);
-            Err(format!("Failed to parse JSON: {}", e))
+            return Err(format!("Failed to parse JSON: {}", e));
         }
     }
+    Ok("success".to_string())
 }
 
 
