ffd1f1e120869e8a2c542a7bff0077ee57bcbfe8
lemonilemon <imlemonilemon@gmail.com>
2024-03-02 11:31:16 +0800

    init navbar (temp)

--- src/utils/navbar/Navbar.tsx
+++ src/utils/navbar/Navbar.tsx
@@ -1,20 +1,39 @@
 import React from "react";
-import { Box } from "@radix-ui/themes";
-import ToggleGroup from "@radix-ui/react-toggle-group";
-export interface IProps {
-  process: string;
-}
-export default function Navbar(props: IProps) {
-  return (
-    <Box>
-      <image>
-        <img
-          src="https://raw.githubusercontent.com/CKEFGISC/MochaGen/main/assets/logo.png"
-          alt="MochaGen Logo"
-          width="600"
-          height="100"
-        />
-      </image>
-    </Box>
-  );
+import ProcessContext from "./ProcessContext";
+import * as ToggleGroup from '@radix-ui/react-toggle-group';
+import { HomeIcon, ReaderIcon, TextAlignLeftIcon, TextAlignCenterIcon, TextAlignRightIcon } from '@radix-ui/react-icons';
+import "./styles.css";
+export default function Navbar() {
+    const { process, setProcess } = React.useContext(ProcessContext);
+    return (
+        <ToggleGroup.Root
+            className="ToggleGroup"
+            type="single"
+            defaultValue={process.toString()}
+            value={process.toString()}
+            onValueChange={(nxt) => setProcess(
+                { type: "set", payload: parseInt(nxt) }
+            )}
+            orientation="vertical"
+            data-orientation="vertical"
+        >
+            <ToggleGroup.Item
+                className="ToggleGroupItem"
+                value="0"
+                aria-label="Home"
+                data-orientation="vertical"
+            >
+                <HomeIcon />
+            </ToggleGroup.Item>
+            <ToggleGroup.Item
+                className="ToggleGroupItem"
+                data-orientation="vertical"
+                value="1"
+                aria-label="Description"
+            >
+                <ReaderIcon />
+            </ToggleGroup.Item>
+
+        </ToggleGroup.Root>
+    );
 }
