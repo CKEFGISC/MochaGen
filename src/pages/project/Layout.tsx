21c04804b177037f89a253b2ec3c2f58a1c0b9dc
AaronWu-train <aaronwu0218@gmail.com>
2024-03-02 14:48:38 +0800

    Change Layout

--- src/pages/project/Layout.tsx
+++ src/pages/project/Layout.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Container, Flex } from "@radix-ui/themes";
+import { Flex } from "@radix-ui/themes";
 import Navbar from "../../utils/navbar/Navbar";
+import ProcessControl from "../../utils/navbar/ProcessControls";
 
 const Layout: React.FC = () => {
   return (
-    <>
+    <Flex
+      display="flex"
+      direction="column"
+      align="center"
+      justify="center"
+      gap="4"
+      style={{
+        height: "100%",
+        width: "100%",
+        flex: "1",
+      }}
+    >
       <Flex
         display="flex"
         direction="row"
@@ -13,19 +25,17 @@ const Layout: React.FC = () => {
         justify="center"
         gap="4"
         style={{
-          height: "calc(100% - 40px)",
           width: "100%",
           flex: "1",
         }}
       >
-        <Flex justify="start">
-          <Navbar />
-        </Flex>
-        <Flex>
-          <Outlet />
-        </Flex>
+        <Navbar />
+        <Outlet />
       </Flex>
-    </>
+      <Flex align="center" width="100%">
+        <ProcessControl />
+      </Flex>
+    </Flex>
   );
 };
 export default Layout;
