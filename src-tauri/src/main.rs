8ca24be6dc86d406f657e7fb0fea3bc06d681fe6
cj.tsai <jimtsai9596@gmail.com>
2024-03-02 15:08:51 +0800

    finish token parser

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -20,6 +20,7 @@ const GENERATOR_POSITION: &str = "gen";
 const TEMPORARY_TOKEN: &str = "/Users/jimtsai/ytp/test";
 const CPP_TEMPLATE_HEAD: &str = "
 #include<bits/stdc++.h>
+#include<assembler.h>
 using namespace std;
 
 signed main(){
@@ -49,7 +50,7 @@ fn parse_token(project_directory: &str) -> Result<String, String> {
     let tokens_all: serde_json::Result<serde_json::Value> = serde_json::from_str(&contents); //parse json
     match tokens_all { //chatgpt 跟我說這樣編譯才會過 好像是因為parse的結果會有一個status code，成功或失敗都要接住，畢竟rust不能出錯嘛
         Ok(parsed_json) => { //parse 過了！
-            println!("Parsed JSON: {:?}", parsed_json); //debug用
+            //println!("Parsed JSON: {:?}", parsed_json); //debug用
             if let Some(tokens_subtasks) = parsed_json["subtasks"].as_array() { //這邊也是要接error
                 let gen_dir = format!("{}/gen", project_directory); // 找到檔案要寫去哪裡
                 if !std::path::Path::new(&gen_dir).exists() {  // 如果檢測他是否已經有路徑 沒有的話就創
@@ -64,22 +65,47 @@ fn parse_token(project_directory: &str) -> Result<String, String> {
                         Ok(file) => file,
                         Err(e) => return Err(format!("Failed to create file '{}': {}", file_name, e)),
                     };
-                    let mut code = CPP_TEMPLATE_HEAD.to_string();
+
+                    let mut code = CPP_TEMPLATE_HEAD.to_string(); 
                     if let Some(tokens_array) = tokens["tokens"].as_array() {
-                        for token_object in tokens_array {
-                            let token_params = &token_object["object"];
+                    for token_object in tokens_array {
+                    let token_params = &token_object["object"];
                             let category = printable(&token_object["object"]["category"]);
                             let variable_name = printable(&token_object["id"]);
                             let class = printable(&token_params["class"]);
-                            code = format!("{}gen_{} {}=gen_{}", code, category, variable_name, category);
-                            // 處理那些constructer們
-                            if category == "number".to_string() {
-                                code = format!("{}(\"{}\")", code, class);
+                            // 處理array的template
+                            if category == "array".to_string() {
+                                code = format!("{}gen_{}<{}> {}=gen", code, category, class, variable_name);
+                                //code = format!("{}({}, {})", code, printable(token_params["len"]), printable(token_params["content"]));
+                            }else{
+                                code = format!("{}gen_{} {}=gen", code, category, variable_name);
                             }
                             //end
                             
                             //處理attr
-
+                            for(index, (key, value)) in token_params["attr"].as_object().unwrap().iter().enumerate(){
+                              if(key.starts_with('_')){code = format!("{}{}(", code, key);}
+                              else {code = format!("{}.{}(", code, key);}
+                              println!("{}",printable(value));
+                              if let Some(attributes_array) = value.as_object() {
+                                for (i, attribute) in attributes_array.iter() {
+                                  if(code.ends_with('(')){
+                                    if(attribute=="integer" ||attribute=="float" || i=="pattern"){
+                                      code = format!("{}{}", code,attribute);
+                                    }else{
+                                      code = format!("{}{}", code,printable(attribute));
+                                  }
+                                  }else{
+                                  code = format!("{},{}", code,printable(attribute));
+                                }
+                                }
+                              }
+                              if code.ends_with(',') {
+                                code.pop();
+                              }
+                              code=format!("{})", code);
+                            }
+                            code=format!("{};\n", code);
                             //end
                         }
                     } else {
@@ -91,7 +117,7 @@ fn parse_token(project_directory: &str) -> Result<String, String> {
                 }
             } else {
                 return Err("Subtasks not found in JSON".to_string());
-            }
+            } 
         }
         Err(e) => {
             eprintln!("Failed to parse JSON: {}", e);
