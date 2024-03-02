c5e96ec6d07a4631de319fd6e55a232e1a9ca9cb
lemonilemon <imlemonilemon@gmail.com>
2024-03-02 15:28:52 +0800

    update indentation

--- src/utils/navbar/Navbar.tsx
+++ src/utils/navbar/Navbar.tsx
@@ -1,39 +1,32 @@
 import React from "react";
 import ProcessContext from "./ProcessContext";
-import * as ToggleGroup from '@radix-ui/react-toggle-group';
-import { HomeIcon, ReaderIcon, TextAlignLeftIcon, TextAlignCenterIcon, TextAlignRightIcon } from '@radix-ui/react-icons';
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
+import {
+  HomeIcon,
+  ReaderIcon,
+  TextAlignLeftIcon,
+  TextAlignCenterIcon,
+  TextAlignRightIcon,
+} from "@radix-ui/react-icons";
 import "./styles.css";
 export default function Navbar() {
-    const { process, setProcess } = React.useContext(ProcessContext);
-    return (
-        <ToggleGroup.Root
-            className="ToggleGroup"
-            type="single"
-            defaultValue={process.toString()}
-            value={process.toString()}
-            onValueChange={(nxt) => setProcess(
-                { type: "set", payload: parseInt(nxt) }
-            )}
-            orientation="vertical"
-            data-orientation="vertical"
-        >
-            <ToggleGroup.Item
-                className="ToggleGroupItem"
-                value="0"
-                aria-label="Home"
-                data-orientation="vertical"
-            >
-                <HomeIcon />
-            </ToggleGroup.Item>
-            <ToggleGroup.Item
-                className="ToggleGroupItem"
-                data-orientation="vertical"
-                value="1"
-                aria-label="Description"
-            >
-                <ReaderIcon />
-            </ToggleGroup.Item>
-
-        </ToggleGroup.Root>
-    );
+  const { process, setProcess } = React.useContext(ProcessContext);
+  return (
+    <ToggleGroup.Root
+      className="ToggleGroup"
+      type="single"
+      defaultValue={process.toString()}
+      value={process.toString()}
+      onValueChange={(nxt) => setProcess({ type: "set", payload: parseInt(nxt) })}
+      orientation="vertical"
+      data-orientation="vertical"
+    >
+      <ToggleGroup.Item className="ToggleGroupItem" value="0" aria-label="Home" data-orientation="vertical">
+        <HomeIcon />
+      </ToggleGroup.Item>
+      <ToggleGroup.Item className="ToggleGroupItem" data-orientation="vertical" value="1" aria-label="Description">
+        <ReaderIcon />
+      </ToggleGroup.Item>
+    </ToggleGroup.Root>
+  );
 }
