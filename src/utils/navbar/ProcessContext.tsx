38e2bf07fe054ece99b5da84886ddf8d9310d432
lemonilemon <imlemonilemon@gmail.com>
2024-03-02 15:28:52 +0800

    update indentation

--- src/utils/navbar/ProcessContext.tsx
+++ src/utils/navbar/ProcessContext.tsx
@@ -1,32 +1,32 @@
 import React, { Dispatch } from "react";
 
 export interface IProcessContext {
-    process: number;
-    setProcess: Dispatch<any>;
+  process: number;
+  setProcess: Dispatch<any>;
 }
 const ProcessContext = React.createContext<IProcessContext>({
-    process: 0,
-    setProcess: () => { }
+  process: 0,
+  setProcess: () => {},
 });
 
 interface IProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const ProcessContextProvider = ({ children }: IProps) => {
-    const [process, setProcess] = React.useReducer((state: number, action: any) => {
-        switch (action.type) {
-            case "next":
-                return state + 1;
-            case "prev":
-                return state - 1;
-            case "set":
-                return action.payload;
-            default:
-                return state;
-        }
-    }, 0);
-    return <ProcessContext.Provider value={{ process, setProcess }}>{children}</ProcessContext.Provider>;
+  const [process, setProcess] = React.useReducer((state: number, action: any) => {
+    switch (action.type) {
+      case "next":
+        return state + 1;
+      case "prev":
+        return state - 1;
+      case "set":
+        return action.payload;
+      default:
+        return state;
+    }
+  }, 0);
+  return <ProcessContext.Provider value={{ process, setProcess }}>{children}</ProcessContext.Provider>;
 };
 
 export default ProcessContext;
