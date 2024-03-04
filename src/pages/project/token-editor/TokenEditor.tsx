e48b3370ce91323fe2c5479d62fe6bf6d8ba1652
AaronWu-train <aaronwu0218@gmail.com>
2024-03-04 22:14:52 +0800

    Create Multi tab token editor
    cherry picked from blockly

--- src/pages/project/token-editor/TokenEditor.tsx
+++ src/pages/project/token-editor/TokenEditor.tsx
@@ -1,10 +1,45 @@
 import BlocklyEditor from "./blockly/BlocklyEditor";
+import React from "react";
+import { Tabs, Text, Box } from "@radix-ui/themes";
+
+// TODO: Replace with real data
+const subtasks = [
+  {
+    name: "subtask1",
+    testcase_count: 5,
+    generator: "subtasks/1/generator.cpp",
+    checker: "subtasks/1/checker.cpp",
+    token: "subtasks/1/token.json",
+    blockly: "subtasks/1/blockly.json",
+  },
+  {
+    name: "subtask2",
+    testcase_count: 5,
+    generator: "subtasks/2/generator.cpp",
+    checker: "subtasks/2/checker.cpp",
+    token: "subtasks/2/token.json",
+    blockly: "subtasks/2/blockly.json",
+  },
+];
 
 const TokenEditor: React.FC = () => {
   return (
-    <div>
-      <BlocklyEditor />
-    </div>
+    <Tabs.Root defaultValue="account">
+      <Tabs.List>
+        {subtasks.map((subtaskContent, subtaskIndex) => (
+          <Tabs.Trigger value={"subtask" + subtaskIndex.toString()}> {subtaskContent.name}</Tabs.Trigger>
+        ))}
+      </Tabs.List>
+
+      <Box>
+        {subtasks.map((subtaskContent, subtaskIndex) => (
+          <Tabs.Content value={"subtask" + subtaskIndex.toString()}>
+            <Text size="2">Subtask {subtaskContent.name}</Text>
+            <BlocklyEditor subtask_key={"subtask" + subtaskIndex.toString()} subtask_json={subtaskContent} />
+          </Tabs.Content>
+        ))}
+      </Box>
+    </Tabs.Root>
   );
 };
 
