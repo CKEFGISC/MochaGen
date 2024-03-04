d2c9aab4f502ba8e0ca10648b2e52438d2bcdb58
lemonilemon <imlemonilemon@gmail.com>
2024-03-04 15:20:34 +0800

    remove redundant part

--- src/pages/project/Layout.tsx
+++ src/pages/project/Layout.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useContext } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 import { Flex } from "@radix-ui/themes";
 import Navbar from "../../utils/navbar/Navbar";
+import "react-toastify/dist/ReactToastify.css";
+
 import ProcessControl from "../../utils/navbar/ProcessControls";
 
 const Layout: React.FC = () => {
