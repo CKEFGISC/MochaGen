88659c6d4549bf07ce66d24d0585520c154e14e6
lemonilemon <imlemonilemon@gmail.com>
2024-03-05 15:18:15 +0800

    init create_project function

--- src-tauri/src/functions/init.rs
+++ src-tauri/src/functions/init.rs
@@ -29,27 +29,31 @@ struct Project {
 
 #[tauri::command]
 pub fn create_project(project_name: &str, project_path: &str) -> Result<String, String>{
+    use serde_json::json;
     let project: Project = Project {
         project_name: String::from(project_name),
         description: String::from(""),
         version: String::from("0.0.1"),
-        cpp_compile_command: "g++".to_string(),
+        cpp_compile_command: String::from("g++"),
         cpp_compile_flags: vec![
-            "-std=c++17".to_string(),
-            "-Wall".to_string(),
-            "-O2".to_string(),
+            String::from("-std=c++17"),
+            String::from("-Wall"),
+            String::from("-O2"),
         ],
         subtask_count: 0,
         subtasks: vec![],
-        build_dir: "build".to_string(),
-        problem_dir: "problem".to_string(),
-        solution_cpp: "problem/solution.cpp".to_string(),
-        testcase_dir: "testcase".to_string(),
-        _comment: "".to_string(),
+        build_dir: String::from("build"),
+        problem_dir: String::from("problem"),
+        solution_cpp: String::from("problem/solution.cpp"),
+        testcase_dir: String::from("testcase"),
+        _comment: String::from(""),
     };
+    crate::functions::json::write_json_to_file(&json!(project), project_path).expect("Problem creating project file.");
     Ok("success".to_string())
 }
 #[tauri::command]
 pub fn load_project(project_path: &str) -> Result<String, String>{
+    crate::functions::json::get_mcg_with_project_directory(project_path); 
+
     Ok("success".to_string())
 }
