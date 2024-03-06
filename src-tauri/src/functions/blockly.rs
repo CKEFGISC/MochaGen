fe97386edc1c6d361bfb1d933fcbf21e2b26fd23
AaronWu-train <aaronwu0218@gmail.com>
2024-03-06 14:46:44 +0800

    Update blockly.rs

--- src-tauri/src/functions/blockly.rs
+++ src-tauri/src/functions/blockly.rs
@@ -1,14 +1,23 @@
+use std::path::Path;
+
 #[tauri::command]
-pub fn load_blockly(project_path: &str) -> Result<String, String>{
+pub fn load_blockly(
+  project_path: &str,
+  blockly_status: &str,
+  subtask_content: &str,
+) -> Result<String, String> {
+  let project_path = Path::new(project_path);
+  let dir_path = project_path.parent().unwrap();
+
   Ok("success".to_string())
 }
 
 #[tauri::command]
-pub fn save_blockly(project_path: &str) -> Result<String, String>{
+pub fn save_blockly(project_path: &str) -> Result<String, String> {
   Ok("success".to_string())
 }
 
 #[tauri::command]
-pub fn save_token(project_path: &str) -> Result<String, String>{
+pub fn save_token(project_path: &str) -> Result<String, String> {
   Ok("success".to_string())
 }
