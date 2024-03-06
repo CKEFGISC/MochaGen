5f2e781542b277c1b7107e0568ae5f7d3b0a1c03
AaronWu-train <aaronwu0218@gmail.com>
2024-03-06 15:01:00 +0800

    Update blockly.rs

--- src-tauri/src/functions/blockly.rs
+++ src-tauri/src/functions/blockly.rs
@@ -1,13 +1,14 @@
 use std::path::Path;
 
+use super::json::get_project_directory_with_config_file;
+
 #[tauri::command]
 pub fn load_blockly(
   project_path: &str,
   blockly_status: &str,
   subtask_content: &str,
 ) -> Result<String, String> {
-  let project_path = Path::new(project_path);
-  let dir_path = project_path.parent().unwrap();
+  let dir_path = get_project_directory_with_config_file(project_path);
 
   Ok("success".to_string())
 }
