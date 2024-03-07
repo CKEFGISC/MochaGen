6c5759d91e86f9e713e71fbab331d3e3a2b88cfd
AaronWu-train <aaronwu0218@gmail.com>
2024-03-07 10:03:49 +0800

    Update Result.tsx

--- src/pages/project/result/Result.tsx
+++ src/pages/project/result/Result.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Flex, Heading, Text } from "@radix-ui/themes";
+import { Button, Flex, Heading, Text, Table, HoverCard } from "@radix-ui/themes";
 
 const Result: React.FC = () => {
   return (
@@ -27,6 +27,44 @@ const Result: React.FC = () => {
           </Heading>
           <Text align="center">Download the generated testdata</Text>
         </Flex>
+        <Button size="4">Generate</Button>
+        <Table.Root style={{ width: "80%" }}>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>index</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Subtask Name</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Testcase Amonut</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Checker Status</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.RowHeaderCell>1</Table.RowHeaderCell>
+              <Table.Cell>sutask1</Table.Cell>
+              <Table.Cell>5</Table.Cell>
+              <Table.Cell>Not Generated</Table.Cell>
+              <Table.Cell>Not Checked</Table.Cell>
+            </Table.Row>
+
+            <Table.Row>
+              <Table.RowHeaderCell>2</Table.RowHeaderCell>
+              <Table.Cell>sutask2</Table.Cell>
+              <Table.Cell>5</Table.Cell>
+              <Table.Cell>Generated</Table.Cell>
+              <Table.Cell>
+                <Flex align="center" gap="2">
+                  <Text color="green">Passed (4/5): 7, 8, 9, 10,</Text>
+                  <Text color="red">Failed (1/5): 6,</Text>
+                </Flex>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table.Root>
+        <Button m="6" size="4">
+          Open Folder
+        </Button>
       </Flex>
     </>
   );
