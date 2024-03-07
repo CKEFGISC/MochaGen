934c070ae4e0952d814fec0b4645e9728655b1dc
lemonilemon <imlemonilemon@gmail.com>
2024-03-07 18:49:06 +0800

    hotfix: parse configPath

--- src/utils/ConfigPathKeeper.ts
+++ src/utils/ConfigPathKeeper.ts
@@ -1,9 +1,20 @@
 const pathKey = (Math.random() + 1).toString(36).substring(7);
 
 export function getConfigPath() {
-  return window.sessionStorage?.getItem(pathKey);
+  return JSON.parse(window.sessionStorage?.getItem(pathKey));
 }
 
 export function setConfigPath(path: String) {
   return window.sessionStorage?.setItem(pathKey, JSON.stringify(path));
+}
+
+import { dirname } from "@tauri-apps/api/path";
+export function getProjectPath() {
+  dirname(getConfigPath())
+    .then((res) => {
+      return res;
+    })
+    .catch((e) => {
+      throw e;
+    });
 }
