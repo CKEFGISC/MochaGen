1fcc16f983d14541cefe9fd83d9fbcad00e521e8
AaronWu-train <aaronwu0218@gmail.com>
2024-03-07 08:42:22 +0800

    Update CodeEditor.tsx

--- src/pages/project/code-editor/CodeEditor.tsx
+++ src/pages/project/code-editor/CodeEditor.tsx
@@ -1,7 +1,28 @@
-import { Flex, Heading } from "@radix-ui/themes";
+import { Flex, Heading, Text, Tabs } from "@radix-ui/themes";
 import Editor, { loader } from "@monaco-editor/react";
 export default function CodeEditor() {
   loader.config({ paths: { vs: "/node_modules/monaco-editor/min/vs" } });
+
+  // TODO: Get subtasks from backend
+  const subtasks = [
+    {
+      name: "subtask1",
+      testcase_count: 5,
+      generator: "subtasks/1/generator.cpp",
+      checker: "subtasks/1/checker.cpp",
+      token: "subtasks/1/token.json",
+      blockly: "subtasks/1/blockly.json",
+    },
+    {
+      name: "subtask2",
+      testcase_count: 5,
+      generator: "subtasks/2/generator.cpp",
+      checker: "subtasks/2/checker.cpp",
+      token: "subtasks/2/token.json",
+      blockly: "subtasks/2/blockly.json",
+    },
+  ];
+
   return (
     <>
       <Flex
@@ -16,12 +37,39 @@ export default function CodeEditor() {
           flex: "1",
         }}
       >
-        <Flex direction="column">
-          <Heading size="8" align="center">
-            SourceCode-Editor <br />
+        <Flex direction="column" gap="2" align="center" width="100%" height="max-content">
+          <Heading
+            size="5"
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Generator and Checker Edit
           </Heading>
-          <Editor height="70vh" width="80vw" theme="vs" defaultLanguage="cpp" />
+          <Text align="center">Edit generator.cpp and checker.cpp here.</Text>
         </Flex>
+
+        <Tabs.Root defaultValue="subtask0">
+          <Tabs.List>
+            {subtasks.map((subtaskContent, subtaskIndex) => (
+              <Tabs.Trigger value={"subtask" + subtaskIndex.toString()}> {subtaskContent.name}</Tabs.Trigger>
+            ))}
+          </Tabs.List>
+          {subtasks.map((subtaskContent, subtaskIndex) => (
+            <Tabs.Content value={"subtask" + subtaskIndex.toString()}>
+              <Flex direction="row" gap="4" m="4" align="center" justify="center">
+                <Flex direction="column" gap="2" align="center" justify="center">
+                  <Text align="center">generator.cpp</Text>
+                  <Editor height="60dvh" width="40vw" theme="vs" defaultLanguage="cpp" />
+                </Flex>
+                <Flex direction="column" gap="2" align="center" justify="center">
+                  <Text align="center">checker.cpp</Text>
+                  <Editor height="60dvh" width="40vw" theme="vs" defaultLanguage="cpp" />
+                </Flex>
+              </Flex>
+            </Tabs.Content>
+          ))}
+        </Tabs.Root>
       </Flex>
     </>
   );
