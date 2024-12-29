842ce7718d21f89fd05099fbfde9a87b2959c93d
AaronWu-train <aaronwu0218@gmail.com>
2024-12-29 18:27:46 +0800

    Try to fix monaco editor error

--- src/pages/project/description/Description.tsx
+++ src/pages/project/description/Description.tsx
@@ -7,6 +7,32 @@ import { invoke } from "@tauri-apps/api/tauri";
 import { toast } from "react-toastify";
 import debounce from "lodash.debounce";
 
+
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === 'json') {
+      return new jsonWorker();
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return new cssWorker();
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return new htmlWorker();
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return new tsWorker();
+    }
+    return new editorWorker();
+  },
+};
+
 export default function Description() {
   const [code, setCode] = useState("");
   const load_with_toast = () => {
@@ -42,7 +68,7 @@ export default function Description() {
     debouncedSave(desc);
   };
 
-  loader.config({ paths: { vs: "/node_modules/monaco-editor/min/vs" } });
+  loader.config({ monaco });
   const options: Object = {
     autoIndent: "full",
     contextmenu: true,
